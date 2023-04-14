2a925603e3c9098f2d0e298407fff6f25b3f0bdc
damien MATHIEU <damien.trot34@gmail.com>
2023-04-14 19:29:05 +0200

    Projet a peu près fini, encore 2 3 fantaisies en css et voila

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import { useQuery } from '@apollo/client';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery } from "@apollo/client";
 import { getAllCharacters } from "./qgl/Query";
+import React from "react";
+import { useState, useRef, useEffect } from 'react'
+import autoAnimate from '@formkit/auto-animate'
 
 function App() {
   const { loading, error, data } = useQuery(getAllCharacters);
-  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,22 +17,61 @@ function App() {
         ) : error ? (
           <p>Error: {error}</p>
         ) : (
-          <div>
+          <div className="container">
+            <h1>Rickypedia</h1>
             {data.characters.results.map((character) => {
-              return ( 
-                <div className='character-box'>
-                  <div className='character-logo' style={{backgroundImage: `url(${character.image})`}}></div>
-                  <p>
-                  {character.name}  
-                  </p>
-                </div>
+              return (
+                    <Affichage character={character}></Affichage>                    
               );
-            })}            
+            })}
           </div>
         )}
       </header>
     </div>
-  );  
+  );
+}
+
+function CharacterInfo(props) {
+    return <p className="textInfo">{props.character.name} is a {props.character.gender.toLowerCase()} and is a member of the {props.character.species.toLowerCase()} species. He is currently {props.character.status.toLowerCase()}. {props.character.name} comes from {props.character.location.name}. </p>;
+}
+
+class CharacterBox extends React.Component {  
+  constructor(props) {
+    super(props);
+    this.character = this.props.character;
+  }
+
+  render() {
+    return (
+      <div className="character-box">
+        <div
+          className="character-logo"
+          style={{ backgroundImage: `url(${this.character.image})` }}
+        ></div>
+        <p>{this.character.name}</p>
+      </div>
+    );
+  }
+}
+
+function Affichage(props){
+  const [show, setShow] = useState(false)
+  const parent = useRef(null)
+
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current)
+  }, [parent])
+
+  const reveal = () => setShow(!show)
+
+    return (
+      <div ref={parent} onClick={reveal} className="containerCharacterBox">
+        <CharacterBox character={props.character}/>
+        { show && <CharacterInfo character={props.character}></CharacterInfo> }
+        
+      </div>
+    )
+  
 }
 
 export default App;
--- src/qgl/Query.js
+++ src/qgl/Query.js
@@ -1,12 +1,16 @@
 import { gql } from "@apollo/client";
 
 export const getAllCharacters = gql`
-  query getAllCharacters {
+    query getAllCharacters {
     characters {
       results {
+        gender
+        status
+        species
         id
         image
         name
+        location{name}
       }
     }
   }
